60706125fa45afe2eb67236c58b592f1d6b6667b
Shreesh <shreesh_patil@infosys.com>
2019-12-31 15:38:33 +0000

    Build files for lmc

---
+++ release_lmc.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+service=lmc
+changed=$(cicd version changed-components --component ${service}=.)
+
+test -z "${changed}" && exit 0
+
+cur_tag=$(cicd version latest-tag --component ${service})
+new_tag=$(cicd version create-tag --component ${service})
+
+echo "Created new release tag ${new_tag}"
+
+# build and publish #
+echo "Making release"
+make -f Makefile_lmc publish
+make -f Makefile_lmc add_info_to_dashboard job_result=SUCCESS
+
+# push the tags
+#git push --tags
+
+# update edge service versions and tag
+#cd ../../
+make -f Makefile_lmc update_service_versions
+#make update_version
