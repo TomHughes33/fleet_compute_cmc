338720dddbf7ece1c2c7fb5ed702b62e8f71ad71
Andrew Noren <andrew.noren@inmarsat.com>
2020-01-21 09:37:58 +0000

    Test versioning

--- release_cmc.sh
+++ release_cmc.sh
@@ -13,7 +13,7 @@ new_tag=$(cicd version create-tag --component ${service})
 echo "Created new release tag ${new_tag}"
 
 # build and publish #
-echo "Making release"
+echo "Making a cmc release"
 make publish service=${service}
 make add_info_to_dashboard service=${service} job_result=SUCCESS
 
@@ -21,6 +21,7 @@ make add_info_to_dashboard service=${service} job_result=SUCCESS
 git push --tags
 
 # update shore service versions and tag
+echo "Making a shore release"
 git clone git@github.com:Inmarsat/fleet_compute.git
 cd fleet_compute
 sed -i "/version_cmc/c\version_${service}: $(new_tag)" shore/provisioning/service_versions.yaml
