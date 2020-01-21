97a6d26bdd452a5973738e06f04b5e59af3e4200
Andrew Noren <andrew.noren@inmarsat.com>
2020-01-21 09:50:30 +0000

    Test versioning

--- release_cmc.sh
+++ release_cmc.sh
@@ -3,6 +3,7 @@
 set -e
 
 service=cmc
+branch=$(git rev-parse --abbrev-ref HEAD)
 changed=$(cicd version changed-components --component ${service}=.)
 
 test -z "${changed}" && exit 0
@@ -24,7 +25,7 @@ git push --tags
 echo "Making a shore release"
 git clone git@github.com:Inmarsat/fleet_compute.git
 cd fleet_compute
-sed -i "/version_cmc/c\version_${service}: ${new_tag}" shore/provisioning/service_versions.yaml
+sed -i "/version_cmc/c\version_${service}: ${new_tag}.0-${branch}" shore/provisioning/service_versions.yaml
 if ! git diff --exit-code -- shore/provisioning/service_versions.yaml; then
     git add shore/provisioning/service_versions.yaml
     git commit -m "Auto update Shore service version file"
