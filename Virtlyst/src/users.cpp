5637f512094162ad02757c614a09bcd3bfb1caf7
naga <naga.sivapuram@infosys.com>
2020-01-27 10:49:29 +0000

    Error message when creating a user with a duplicate username

--- Virtlyst/src/users.cpp
+++ Virtlyst/src/users.cpp
@@ -119,7 +119,7 @@ bool Users::find(const QString &username, const QString &id)
 
 void Users::edit(Context *c, const QString &id)
 {
-    qWarning() << "ID: " + id;
+    qDebug() << "ID: " + id;
     if (c->request()->isPost()) {
         const ParamsMultiMap params = c->req()->bodyParameters();
 	if (!find(params.value(QStringLiteral("username")),id)) {
